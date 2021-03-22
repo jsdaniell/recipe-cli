a216c5b2b508b5f8b04b29e152d1c68e125c62a0
Daniel <jose.daniell@outlook.com>
2021-03-21 21:10:13 -0300

    Fix Firebase Model Generation

--- cmd/golang/golang_api/go_content_files/api_models_files.go
+++ cmd/golang/golang_api/go_content_files/api_models_files.go
@@ -21,7 +21,9 @@ func CreateModelsPackage(projectName, projectDatabase string) {
 }
 
 func writeEntityModelFile(projectName, projectDatabase string){
-	var contentFirebase = `// Entity model used to receive request and send JSON response, is used on authentication controller.
+	var contentFirebase = `package models
+
+// Entity model used to receive request and send JSON response, is used on authentication controller.
 type Entity struct {
 	ID            string   ` + "`" + `json:"_id"` + "`" + `
 	Name          string   ` + "`" + `json:"name, omitempty"` + "`" + `
