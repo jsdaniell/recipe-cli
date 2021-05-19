4a1ec332fee4195bb22ec646cf4b8adef92f5eb9
Daniel <jose.daniell@outlook.com>
2021-05-19 15:26:24 -0300

    update

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,3 +1,4 @@
 go build -o bin/recipe-cli -v .
 git commit -am "update"
-git tag -a v1.1.5 -m "Alpha Release" && git push origin v1.1.5
+git tag -a v1.1.6 -m "Alpha Release" && git push origin v1.1.6
+npm publish
